8ddd632898358c55f34b40153a3861f687f8364a
antoinepieren <antoine.pieren@etu.emse.fr>
2023-05-30 23:24:02 +0200

    better init

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ First of all, to make sure you've got all the librairies installed, run
  `pip install -r requirements.txt`
 
 Then you can run ***init.py*** py double-clicking on ***init.ps1*** (only do so after you checked the contents of both files, don't trust everything you got from the internet)
+
+This will create the webtoon.sqlite database
+
+//TODO : Discord bot setup tutorial with json file and all
--- init.py
+++ init.py
@@ -1,6 +1,8 @@
 from sql import *
 
+# TODO : Check if empty with SELECT name FROM sqlite_schema WHERE type='table'
 connect("webtoon.sqlite") # Creating database if it doesn't exist yet
 #request("DROP TABLE names")
 request("CREATE TABLE names (id INT PRIMARY KEY,main TEXT,alt TEXT)") # Table for webtoon with multiple names because of translation
-request("CREATE TABLE list (title TEXT,interest TEXT,status TEXT,last_out INT,last_read INT)")
+request("CREATE TABLE list (id INT PRIMARY KEY,title TEXT,interest TEXT,status TEXT,last_out INT,last_read INT)") # Table for the list of different webtoons and their charachteristics
+request("CREATE TABLE chapters (id INT PRIMARY KEY,title TEXT,chapter TEXT,link TEXT)") # Table for all the chapters detected
